df273f11446bbdf4651026cdb27086df6e7afa82
peHaick <pedrohaickzz@gmail.com>
2020-09-03 10:18:18 -0300

    Implementadas as funçoes de printar distancia e caminho entre estaçoes

--- projeto-grafos1/grafo.py
+++ projeto-grafos1/grafo.py
@@ -17,8 +17,9 @@ nomes = {  # Linha Azul
     45: 'Cabo Ruivo', 46: 'Olivais', 47: 'Chelas', 48: 'Bela Vista', 49: 'Olaias'
 }
 
-grafo={}
-numVertices=0
+grafo = {}
+numVertices = 0
+
 
 def adcVertice(vertice):
     global grafo
@@ -32,18 +33,20 @@ def adcAresta(vertice1, vertice2):
     grafo[vertice1].append(vertice2)
     grafo[vertice2].append(vertice1)
 
+
 def printGrafo(grafo):
     for node in grafo:
         print("{}: {}".format(node, grafo[node]))
-    print(' ')  
+    print(' ')
 
-for v in range(1,50):
+
+for v in range(1, 50):
     adcVertice(v)
 
-for azul in range(1,18):
+for azul in range(1, 18):
     adcAresta(azul, azul+1)
 
-for amarelo in range(19,29):
+for amarelo in range(19, 29):
     adcAresta(amarelo, amarelo+1)
 
 
@@ -52,17 +55,78 @@ adcAresta(13, 30)
 adcAresta(31, 24)
 adcAresta(24, 32)
 
-for verde in range(32,39):
+for verde in range(32, 39):
     adcAresta(verde, verde+1)
 
 adcAresta(39, 16)
 adcAresta(16, 40)
 
-for vermelho in range(41,49):
+for vermelho in range(41, 49):
     adcAresta(vermelho, vermelho+1)
 
 adcAresta(49, 35)
 adcAresta(35, 28)
 adcAresta(28, 11)
 
-printGrafo(grafo)
+
+def distBFS(inicio, destino):
+    global grafo
+    fila = []
+    visitados = {}
+    for node in grafo:
+        visitados[node] = False
+    visitados[inicio] = True
+    dist = {inicio: 0}
+    fila.append(inicio)
+    while fila:
+        atual = fila.pop(0)
+        for i in grafo[atual]:
+            if not visitados[i]:
+                visitados[i] = True
+                dist[i] = dist[atual]+1
+                fila.append(i)
+    try:
+        return dist[destino]
+    except:
+        return None
+
+
+def printCaminho(inicio, destino):
+    global grafo
+    fila = []
+    visitados = {}
+    for node in grafo:
+        visitados[node] = False
+    visitados[inicio] = True
+    antecessor = {inicio: None}
+    fila.append(inicio)
+    while fila:
+        atual = fila.pop(0)
+        for i in grafo[atual]:
+            if not visitados[i]:
+                visitados[i] = True
+                antecessor[i] = atual
+                fila.append(i)
+    caminho = []
+    atual = destino
+    while atual:
+        caminho.append(atual)
+        atual = antecessor[atual]
+    caminho = caminho[::-1]
+    for estacao in caminho:
+        if estacao == caminho[-1]:
+            print('[{}] {}'.format(estacao, nomes[estacao]))
+        else:
+            print('[{}] {}'.format(estacao, nomes[estacao]), end=' -> ')
+
+
+def printDistancia(inicio, destino):
+    print('[{}] {} -> [{}] {}: {} estações\n'.format(inicio, nomes[inicio],
+                                                     destino, nomes[destino], distBFS(inicio, destino)))
+
+
+# printGrafo(grafo)
+
+
+printDistancia(1, 49)
+printCaminho(1, 49)
